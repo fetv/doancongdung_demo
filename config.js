8abb7d8dfad054681a751124903ed56201ffc0a3
fetv <99081600+fetv@users.noreply.github.com>
2022-11-17 22:14:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Demo Chưa Nghĩ Ra Tên Web",
+    introTitle: 'Đừng Đọc',
+    introDesc: `Anh là một con người thẳng thắn,
+    Sinh ra đã được định sẵn phải yêu em`,
+    btnIntro: 'Tiêp tục nào:3',
+    title: 'Trả lời thật nhé',
+    desc: 'Cậu yêu mình ko :(( ',
+    btnYes: 'Tất Nhiên <33',
+    btnNo: 'Cút:))',
+    question: 'Cho biết lý do đi nào',
+    btnReply: 'Gửi cho người yêu tương lai',
+    reply: 'Cậu đẹp trai quá hà',
+    mess: ':)))))',
+    messDesc: 'ơ kìa ngại quá .',
+    btnAccept: 'Tiếp tục',
+    messLink: 'https://www.facebook.com/profile.php?id=100086705552626' 
 }
